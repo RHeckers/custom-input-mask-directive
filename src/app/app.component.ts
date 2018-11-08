97daff2c72bc2879f5aba6cf964d7f96843d172f
R Heckers <robertoheckers@gmail.com>
2018-11-08 12:21:16 +0100

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'input-mask-directive';
+
+  container:HTMLElement;
+  
+  ngOnInit(){
+    this.container = document.getElementById('container');
+    this.container.style.height = window.innerHeight / 2 + 'px';
+  }
 }
