211ad3b38c4cae76eab50f2ad922bfdb8c48bec3
R Heckers <robertoheckers@gmail.com>
2018-11-08 12:06:14 +0100

    x

--- src/app/input-mask/input-mask.directive.ts
+++ src/app/input-mask/input-mask.directive.ts
@@ -23,105 +23,105 @@ import { maskDigitValidators, neverValidator } from "./digit_validator";
   selector: "[appInputMask]"
 })
 export class InputMaskDirective implements OnInit {
-  @Input("appInputMask") mask = "";
-
-  input: HTMLInputElement;
-
-  constructor(el: ElementRef) {
-    this.input = el.nativeElement;
-  }
-
-  ngOnInit() {
-    console.log(this.input);
-    this.input.value = this.buildPlaceholder();
-  }
-
-  @HostListener("keydown", ["$event", "$event.keyCode"])
-  onKeyDown(event: KeyboardEvent, keyCode) {
-    if (keyCode !== TAB) {
-      event.preventDefault();
+    @Input("appInputMask") mask = "";
+  
+    input: HTMLInputElement;
+  
+    constructor(el: ElementRef) {
+      this.input = el.nativeElement;
     }
-
-    const key = String.fromCharCode(keyCode);
-    const currentCursorPos = this.input.selectionStart;
-
-    switch (keyCode) {
-      case LEFT_ARROW:
-        this.handleLeftArrow(currentCursorPos);
-        return;
-
-      case RIGHT_ARROW:
+  
+    ngOnInit() {
+      console.log(this.input);
+      this.input.value = this.buildPlaceholder();
+    }
+  
+    @HostListener("keydown", ["$event", "$event.keyCode"])
+    onKeyDown(event: KeyboardEvent, keyCode) {
+      if (keyCode !== TAB) {
+        event.preventDefault();
+      }
+  
+      const key = String.fromCharCode(keyCode);
+      const currentCursorPos = this.input.selectionStart;
+  
+      switch (keyCode) {
+        case LEFT_ARROW:
+          this.handleLeftArrow(currentCursorPos);
+          return;
+  
+        case RIGHT_ARROW:
+          this.handleRightArrow(currentCursorPos);
+          return;
+  
+        case BACKSPACE:
+          this.handleBackspace(currentCursorPos);
+          return;
+  
+        case DELETE:
+          this.handleDelete(currentCursorPos);
+          return;
+      }
+  
+      const maskDigit = this.mask.charAt(currentCursorPos);
+      const digitValidator = maskDigitValidators[maskDigit] || neverValidator;
+  
+      if (digitValidator(key)) {
+        overWriteCharAtPos(this.input, currentCursorPos, key);
         this.handleRightArrow(currentCursorPos);
-        return;
-
-      case BACKSPACE:
-        this.handleBackspace(currentCursorPos);
-        return;
-
-      case DELETE:
-        this.handleDelete(currentCursorPos);
-        return;
+      }
     }
 
-    const maskDigit = this.mask.charAt(currentCursorPos);
-    const digitValidator = maskDigitValidators[maskDigit] || neverValidator;
-
-    if (digitValidator(key)) {
-      overWriteCharAtPos(this.input, currentCursorPos, key);
-      this.handleRightArrow(currentCursorPos);
+    handleBackspace(currentCursorPos) {
+      const prevPos = this.calcPrevCursorPos(currentCursorPos);
+  
+      if (prevPos >= 0) {
+        overWriteCharAtPos(this.input, prevPos, "_");
+        this.input.setSelectionRange(prevPos, prevPos);
+      }
     }
-  }
 
-  handleBackspace(currentCursorPos) {
-    const prevPos = this.calcPrevCursorPos(currentCursorPos);
-
-    if (prevPos >= 0) {
-      overWriteCharAtPos(this.input, prevPos, "_");
-      this.input.setSelectionRange(prevPos, prevPos);
+    handleDelete(currentCursorPos){
+      overWriteCharAtPos(this.input, currentCursorPos, '_');
+      this.input.setSelectionRange(currentCursorPos, currentCursorPos)
     }
-  }
-
-  handleDelete(currentCursorPos){
-    overWriteCharAtPos(this.input, currentCursorPos, '_');
-    this.input.setSelectionRange(currentCursorPos, currentCursorPos)
-  }
-
-  handleLeftArrow(currentCursorPos) {
-    const prevPos = this.calcPrevCursorPos(currentCursorPos);
-
-    if (prevPos >= 0) {
-      this.input.setSelectionRange(prevPos, prevPos);
+  
+    handleLeftArrow(currentCursorPos) {
+      const prevPos = this.calcPrevCursorPos(currentCursorPos);
+  
+      if (prevPos >= 0) {
+        this.input.setSelectionRange(prevPos, prevPos);
+      }
     }
-  }
-
-  calcPrevCursorPos(currentCursorPos) {
-    const valueBeforeCursor = this.input.value.slice(0, currentCursorPos);
-    return findLastIndex(
-      valueBeforeCursor,
-      char => !includes(SPECIAL_CHARS, char)
-    );
-  }
-
-  handleRightArrow(currentCursorPos) {
-    const valueAfterCursor = this.input.value.slice(currentCursorPos + 1);
-    const nextPos = findIndex(
-      valueAfterCursor,
-      char => !includes(SPECIAL_CHARS, char)
-    );
-
-    if (nextPos >= 0) {
-      const newCursorPos = currentCursorPos + nextPos + 1;
-      this.input.setSelectionRange(newCursorPos, newCursorPos);
+  
+    calcPrevCursorPos(currentCursorPos) {
+      const valueBeforeCursor = this.input.value.slice(0, currentCursorPos);
+      return findLastIndex(
+        valueBeforeCursor,
+        char => !includes(SPECIAL_CHARS, char)
+      );
     }
-  }
-
-  buildPlaceholder(): string {
-    const chars = this.mask.split("");
-
-    const value = chars.reduce((result, char) => {
-      return (result += includes(SPECIAL_CHARS, char) ? char : "_");
-    }, "");
-
-    return value;
-  }
+  
+    handleRightArrow(currentCursorPos) {
+      const valueAfterCursor = this.input.value.slice(currentCursorPos + 1);
+      const nextPos = findIndex(
+        valueAfterCursor,
+        char => !includes(SPECIAL_CHARS, char)
+      );
+  
+      if (nextPos >= 0) {
+        const newCursorPos = currentCursorPos + nextPos + 1;
+        this.input.setSelectionRange(newCursorPos, newCursorPos);
+      }
+    }
+  
+    buildPlaceholder(): string {
+      const chars = this.mask.split("");
+  
+      const value = chars.reduce((result, char) => {
+        return (result += includes(SPECIAL_CHARS, char) ? char : "_");
+      }, "");
+  
+      return value;
+    }
 }
